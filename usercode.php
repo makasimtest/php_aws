ab053a19cfcdc1dc404cb646531c5c5dbf468931
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2020-02-19 10:56:35 +0200

    init

---
+++ usercode.php
@@ -0,0 +1,32 @@
+<?php
+
+use Aws\S3\S3Client;
+
+require_once __DIR__.'/vendor/autoload.php';
+
+function handle($data) {
+    $s3 = createS3($data['s3']['key'], $data['s3']['secret']);
+
+    $data['result'] = $s3->putObject([
+        'Content-Type'      => 'text/plain',
+        'Bucket'            => 'gitcalltest',
+        'Key'               => time().'.json',
+        'Body'              => json_encode($data['content']),
+        'ACL'               => 'public-read',
+    ]);
+
+    return $data;
+}
+
+function createS3(string $key, string $secret): S3Client {
+    return new S3Client([
+        'version' => 'latest',
+        'region' => 'ams3',
+        'endpoint' => 'https://fra1.digitaloceanspaces.com',
+        'credentials' => [
+            'key'    => $key,
+            'secret' => $secret,
+        ],
+        'debug' => true
+    ]);
+}
